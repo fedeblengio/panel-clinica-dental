ee00a48510a7e91e23d7f78c386dcb158c5f16ed
Federico <fedeeblengio@gmail.com>
2026-03-20 21:52:13 -0300

    Increase content area width and reduce padding for more usable space

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -159,7 +159,7 @@ export function Layout({ children, onLogout }) {
       </aside>
 
       <main className="flex-1 overflow-auto pt-14 md:pt-0 md:ml-16">
-        <div className="max-w-6xl mx-auto p-4 sm:p-6 lg:p-12">
+        <div className="max-w-7xl mx-auto p-4 sm:p-6">
           {children}
         </div>
       </main>
